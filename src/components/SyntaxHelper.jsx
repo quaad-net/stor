e614d88d0f710a23d5e70e100d1651fb65d586c0
unknown <eukoh@quaad.net>
2025-06-13 12:00:48 -0500

    Modified loc query

--- src/components/SyntaxHelper.jsx
+++ src/components/SyntaxHelper.jsx
@@ -11,23 +11,30 @@ export default function SyntaxHelper(props){
                 <div>
                     <div><i>*All queries are case-insensitive.</i></div>
                     <h3>Loc Query</h3>
-                    <div style={{color: 'gold'}}>[Q]&nbsp;LocationStart&nbsp;<span style={{color:'white', fontWeight: 'bold'}}>:</span>&nbsp;LocationEnd</div>
-                    <div>- Example&nbsp;=&gt;&nbsp;<span style={{color: 'gray'}}>110-a-b:110-c</span></div>
-                    <div>- Add location start and location end separted by a colon. 
-                        <br/>To match only one location, enter the location without a colon.
+                    <div style={{color: 'gold'}}>[Q]&nbsp;LocationStart&nbsp;<span style={{color:'white', fontWeight: 'bold'}}>:</span>&nbsp;LocationEnd
+                        <br/><span style={{color: 'white'}}>=&gt;&nbsp;</span><span style={{color: 'gray'}}>110-a-b:110-c</span>
+                    </div>
+                    <div>▫ Add location start and location end separted by a colon. 
+                        <br/>To match only one location, enter the exact location without a colon.
+                    </div>
+                    <div>▫ Do not include any spaces.</div>
+                    <div>▫ Add &quot;&active&quot; to only include active items
+                        <br/>=&gt;&nbsp;<span style={{color: 'gray'}}>110-a-b:110-c&active</span>
                     </div>
                     <h3>Code Query</h3>
-                    <div style={{color: 'gold'}}>[Q]&nbsp;Code&nbsp;Code Code...</div>
-                    <div>- Example&nbsp;=&gt;&nbsp;<span style={{color: 'gray'}}>22-12345 51-12345 70-12345</span></div>
-                    <div>- Space separted list of part codes.</div>
+                    <div style={{color: 'gold'}}>[Q]&nbsp;Code&nbsp;Code Code...
+                        <br/><span style={{color: 'white'}}>=&gt;</span>&nbsp;<span style={{color: 'gray'}}>22-12345 51-12345 70-12345</span>
+                    </div>
+                    <div>▫ Enter a space separted list of part codes.</div>
                     <h3>Descr Query</h3>
-                    <div style={{color: 'gold'}}>[Q]&nbsp;Exact Match</div>
-                    <div>- Example&nbsp;=&gt;&nbsp;<span style={{color: 'gray'}}>zurn kit</span></div>
-                    <div>- Searches for exact match within the &ldquo;description&rdquo; field.</div>
+                    <div style={{color: 'gold'}}>[Q]&nbsp;Exact Match
+                        <br/><span style={{color: 'white'}}>=&gt;</span>&nbsp;<span style={{color: 'gray'}}>zurn kit</span>
+                    </div>
+                    <div>▫ Searches for exact match within the &ldquo;description&rdquo; field.</div>
                     <h3>Using Filter</h3>
-                    <div>- Narrows results based on filter input and type selection.</div>
-                    <div>- Returns records that contain an exact match.</div>
-                    <div>- Allows aggregated filtering.</div>
+                    <div>▫ Narrows results based on filter input and type selection.</div>
+                    <div>▫ Returns records that contain an exact match.</div>
+                    <div>▫ Allows aggregated filtering.</div>
                 </div>
             </>
         )
