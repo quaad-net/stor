b938dc3aff37b458e9435b4d3b03a0893086d6c0
unknown <eukoh@quaad.net>
2026-02-06 02:56:28 -0600

    ...

--- app/imgMap.js
+++ app/imgMap.js
@@ -5,8 +5,6 @@ const imgMap = new Map();
 // Web Provided Images
 imgMap.set('ai-generated-image.svg', `${imgServer}ef8dcbae-8822-4bb6-2871-30194de20400/icon`);
 imgMap.set('calendar-circled-date.svg', `${imgServer}2e820a51-189e-49b2-20a2-a47bdaaaa200/icon`);
-imgMap.set('circled-check.svg', `${imgServer}234910ca-3ea0-47a5-4d93-4589b5cc8900/icon`); 
-imgMap.set('circled-check-red.svg', `${imgServer}99ff1897-b735-41a9-2b89-86c705ffac00/icon`); 
 imgMap.set('comment-blue-2.svg', `${imgServer}617932f3-e74b-4d6e-5d9c-5c6b326e9e00/icon`); 
 imgMap.set('info.svg', `${imgServer}7921d4d9-1304-40cb-eff9-791a17c65a00/icon`); 
 imgMap.set('login.svg', `${imgServer}582cfde1-bf59-4f5a-c3b1-35e6f0941b00/icon`); 
@@ -18,32 +16,34 @@ imgMap.set('pulsar-download.svg', `${imgServer}42c48a4f-5062-4e77-0c69-6b610c86c
 imgMap.set('pulsar-login.svg', `${imgServer}f1a9891e-f1e8-4dc3-d085-18d093d99400/icon`); 
 imgMap.set('pulsar-print.svg', `${imgServer}2c23b09c-dc25-43bc-aa6a-af7d7c06c900/icon`); 
 imgMap.set('stor-fiscal-icon-white.svg', `${imgServer}c96d1729-7a0e-4d94-b8eb-6d228b0fb700/icon`); 
-imgMap.set('stor-logo.svg', `${imgServer}39fbd164-df89-4855-0924-be78ea37f100/icon`); 
-imgMap.set('stor-tab-icon.svg', `${imgServer}b03c3ce2-c616-41da-aa04-fe081eb84a00/icon`);
 imgMap.set('tool-box.svg', `${imgServer}86ba29d5-0c6c-48c1-b7f4-192986ccee00/icon`);
 
 // App Provided Images
 imgMap.set('1-2-3.svg', `/1-2-3.svg`)
-imgMap.set('ai-generated-text.svg', `/ai-generated-text.svg`);
+imgMap.set('ai-generated-text.svg', `/ai-generated-text.png`);
+imgMap.set('circled-check.svg', `/circled-check.png`); 
+imgMap.set('circled-check-red.svg', `/circled-check-red.png`); 
 imgMap.set('checklist.svg', `/checklist.svg`); 
 imgMap.set('closed-hex.png', `/closed-hex.png`);
-imgMap.set('copy.svg', `/copy.svg`); 
-imgMap.set('database-update.svg', `/database-update.svg`); 
-imgMap.set('left-circled-arrow.svg', `/left-circled-arrow.svg`);
-imgMap.set('info-circled.svg', `/info-circled.svg`); 
+imgMap.set('copy.svg', `/copy.png`); 
+imgMap.set('database-update.svg', `/database-update.png`); 
+imgMap.set('left-circled-arrow.svg', `/left-circled-arrow.png`);
+imgMap.set('info-circled.svg', `/info-circled.png`); 
 imgMap.set('open-hex.png', `/open-hex.png`);
 imgMap.set('pulsar-cart.svg', `/pulsar-cart.svg`);
-imgMap.set('pulsar-circled-info.svg', `/pulsar-circled-info.svg`); 
-imgMap.set('pulsar-clear.svg', `/pulsar-clear.svg`); 
+imgMap.set('pulsar-circled-info.svg', `/pulsar-circled-info.png`); 
+imgMap.set('pulsar-clear.svg', `/pulsar-clear.png`); 
 imgMap.set('pulsar-labels.svg', `/pulsar-labels.svg`); 
 imgMap.set('pulsar-layers.svg', `/pulsar-layers.svg`);
 imgMap.set('pulsar-purchase-order.svg', `/pulsar-purchase-order.svg`);
 imgMap.set('pulsar-qr.svg', `/pulsar-qr.svg`); 
 imgMap.set('pulsar-register.svg', `/pulsar-register.svg`); 
 imgMap.set('pulsar-sort.svg', `/pulsar-sort.svg`); 
-imgMap.set('right-circled-arrow.svg', `/right-circled-arrow.svg`);
+imgMap.set('right-circled-arrow.svg', `/right-circled-arrow.png`);
 imgMap.set('skip.svg', `/skip.svg`);
-imgMap.set('square-outlined-small.svg', `/square-outlined-small.svg`); 
+imgMap.set('square-outlined-small.svg', `/square-outlined-small.png`);
+imgMap.set('stor-logo.svg', `/stor-logo.svg`); 
+imgMap.set('stor-tab-icon.svg', `/stor-tab-icon.svg`); 
 imgMap.set('user-small.svg', `/user-small.svg`); 
 
 export default imgMap
